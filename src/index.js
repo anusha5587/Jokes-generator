27dc844953fca28b5e888c74960ec384c866c549
anusha5587 <anoosha.anand.24@gmail.com>
2024-02-12 17:11:02 +0000

    Made final changes to CSS and made the page responsive

--- src/index.js
+++ src/index.js
@@ -15,8 +15,12 @@ function generatePoem(event) {
   let apiKey = "793aeaaa61tcbf2c55f5o4cf70867471";
   let prompt = `User instructions: Generate a poem ${instructionsInput.value}`;
   let context =
-    "You are a romantic poem expert and love to write short poems. Your mission is to generate a 4 line poem in basic HTML and seperate each line with a <br />. Make sure to follow the user instructions. Do not include a title to the poem. Sign the poem with `SheCodes AI` inside a <strong> element at the end of the poem and NOT at the begining";
+    "You are a romantic poem expert and love to write short poems. Your mission is to generate a 4 line poem in basic HTML and seperate each line with a <br />. Make sure to follow the user instructions. Do not include a title to the poem. Sign the poem with `SheCodes AI` inside a <strong> element at the end of the poem after a <br />and NOT at the begining";
   let apiURL = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
+
+  let poemElement = document.querySelector("#poem");
+  poemElement.classList.remove("hidden");
+  poemElement.innerHTML = `<div class="waiting">⏳ Generating a poem about ${instructionsInput.value}</div>`;
 
   console.log("Generating poem");
   console.log(`Prompt: ${prompt}`);
